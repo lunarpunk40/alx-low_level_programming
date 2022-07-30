6fbf7eed224563376f3b7a7bdb1bbd9066698aff
lunarpunk40 <gg1214493@gmail.com>
2022-07-30 11:08:04 -0700

    brtty aprovale

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -192,21 +192,21 @@ int main(int argc, char *argv[])
 char *final_prod, *next_prod;
 int size, index, digit, zeroes = 0;
 
- if (argc != 3)
- {
- printf("Error\n");
- exit(98);
- }
+if (argc != 3)
+{
+printf("Error\n");
+exit(98);
+}
 
- if (*(argv[1]) == '0')
- argv[1] = iterate_zeroes(argv[1]);
- if (*(argv[2]) == '0')
- argv[2] = iterate_zeroes(argv[2]);
- if (*(argv[1]) == '\0' || *(argv[2]) == '\0')
- {
- printf("0\n");
- return (0);
- }
+if (*(argv[1]) == '0')
+argv[1] = iterate_zeroes(argv[1]);
+if (*(argv[2]) == '0')
+argv[2] = iterate_zeroes(argv[2]);
+if (*(argv[1]) == '\0' || *(argv[2]) == '\0')
+{
+printf("0\n");
+return (0);
+}
 
 size = find_len(argv[1]) + find_len(argv[2]);
 final_prod = create_xarray(size + 1);
