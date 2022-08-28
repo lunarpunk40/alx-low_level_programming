bd32732af10bd94d449e339b3d0d6b775052cba0
lunarpunk40 <gg1214493@gmail.com>
2022-08-28 09:10:21 -0700

    update

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,3 +1,8 @@
+/*
+ * File: 3-cp.c
+ * Auth: Gigi
+ */
+
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
@@ -60,6 +65,7 @@ int main(int argc, char *argv[])
 {
 int from, to, r, w;
 char *buffer;
+
 if (argc != 3)
 {
 dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
@@ -84,10 +90,11 @@ w = write(to, buffer, r);
 if (to == -1 || w == -1)
 {
 dprintf(STDERR_FILENO,
-"Error: Can't write to %s\n", argv[2])
+"Error: Can't write to %s\n", argv[2]);
 free(buffer);
 exit(99);
 }
+
 r = read(from, buffer, 1024);
 to = open(argv[2], O_WRONLY | O_APPEND);
 
